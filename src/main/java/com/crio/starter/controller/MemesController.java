e06f7a9759b05a410bb0715daa1474a8ede30ee2
Lavina Chotrani <chotranilavina@gmail.com>
2021-08-30 16:59:23 +0530

    Update MemesController.java

--- src/main/java/com/crio/starter/controller/MemesController.java
+++ src/main/java/com/crio/starter/controller/MemesController.java
@@ -4,18 +4,13 @@ import com.crio.starter.data.*;
 import com.crio.starter.exchange.*;
 import com.crio.starter.repository.*;
 import com.crio.starter.service.*;
-import com.crio.starter.service.MemesService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
-
 import lombok.extern.log4j.Log4j2;
 
-import java.util.Map;
-import java.util.List;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Stream;
 import java.time.LocalDateTime;
 
@@ -24,11 +19,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.validation.*;
 
@@ -44,119 +34,56 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-
+import org.springframework.stereotype.Controller;
 
 @Log4j2
-@RestController
+@Controller
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class MemesController {
-  @Autowired
-  UserRepository userRepository;
-  @Autowired
-  private final MemesService memesService;
-  @Autowired
-  private UserService userService;
-  private Map<String, LocalDateTime> usersLastAccess = new HashMap<>();
+@Autowired
+UserRepository userRepository;
+@Autowired
+private final MemesService memesService;
+@Autowired
+private UserService userService;
+private Map<String, LocalDateTime> usersLastAccess = new HashMap<>();
 
-  @GetMapping("/")
-  public RedirectView home() {
-    return new RedirectView("/home.html");
-  }
+@GetMapping("/")
+public RedirectView home() {
+        return new RedirectView("/home.html");
+}
 
-  @GetMapping("/user/login")
- public RedirectView getCurrentUser(@AuthenticationPrincipal @Validated User user, Model model) {
-       String username = user.getUsername();
-       model.addAttribute("username", username);
-       model.addAttribute("lastAccess", usersLastAccess.get(username));
-       usersLastAccess.put(username, LocalDateTime.now());
-       return new RedirectView("/");
- }
+@GetMapping("/login")
+public RedirectView getCurrentUser(@AuthenticationPrincipal @Validated User user, Model model) {
+        String username = user.getUsername();
+        model.addAttribute("username", username);
+        model.addAttribute("lastAccess", usersLastAccess.get(username));
+        usersLastAccess.put(username, LocalDateTime.now());
+        return new RedirectView("/");
+}
 
-    @GetMapping("/register")
-        public RedirectView register(final Model model){
-          log.info("Get Register");
-            model.addAttribute("userData", new UserData());
-            return new RedirectView("register.html");
-        }
-    //
-    //     @PostMapping("/register")
-    //     public RedirectView userRegistration(@Valid @ModelAttribute("userForm") UserData userData, final BindingResult bindingResult, final Model model){
-    //       log.info("Post register");
-    //         if(bindingResult.hasErrors()){
-    //             model.addAttribute("registrationForm", userData);
-    //             return new RedirectView("register.html");
-    //         }
-    //         try {
-    //             userService.register(userData);
-    //         }catch (DataIntegrityViolationException e){
-    //             bindingResult.rejectValue("email", "userData.email","An account already exists for this email.");
-    //             model.addAttribute("registrationForm", userData);
-    //             return new RedirectView("register.html");
-    //         }
-    //         userService.register(userData);
-    //         return new RedirectView("/");
-      //  }
+@GetMapping("/register")
+public RedirectView register(final Model model){
+        log.info("Get Register");
+        model.addAttribute("userData", new UserData());
+        return new RedirectView("register.html");
+}
+@RequestMapping("/index")
+public String index() {
+        return "index";
+}
 
-  @GetMapping("/memes")
-  public List<MemesEntity> getMemes() {
-
-    log.info("getMemes called");
-    getResponseMeme getMemeResponse;
-    getMemeResponse = memesService.getAllMemes();
-
-    return getMemeResponse.getMemes();
-  }
-
-  @GetMapping("/memes/{memeId}")
-  public MemesEntity getMeme(@PathVariable String memeId) {
-
-    log.info("getMeme called with {}", memeId);
-    MemesEntity getMemeResponse;
-    getMemeResponse = memesService.getMemes(memeId);
-
-    return getMemeResponse;
-  }
-
-  @PostMapping("/register")
-  public String registerUser(@Validated @RequestBody String userJson) {
-    log.info("Registering" + userJson);
-    ObjectMapper objectMapper = new ObjectMapper();
-    UserData userData = new UserData();
-    try {
-      userData = objectMapper.readValue(userJson, UserData.class);
-      log.info(userData.getUsername());
-      userService.register(userData);
-    } catch (JsonProcessingException e) {
-      e.printStackTrace();
-    }
-
-    if(Stream.of(userData.getPassword(),userData.getUsername()).allMatch(Objects::isNull))
-    throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-    log.info("registered");
-    return "Success";
-  }
-
-  @PostMapping("/memes")
-  public getPostResponse postMeme(@Validated @RequestBody String memeJson)  {
-    log.info("postMeme called with {}", memeJson);
-    ObjectMapper objectMapper = new ObjectMapper();
-    MemesEntity meme = new MemesEntity();
-    try {
-      meme = objectMapper.readValue(memeJson, MemesEntity.class);
-    } catch (JsonProcessingException e) {
-      e.printStackTrace();
-    }
-    if(Stream.of(meme.getName(),meme.getUrl(),meme.getCaption()).allMatch(Objects::isNull))
-    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Can't post empty object");
-    return memesService.postMeme(meme);
-  }
-
-  @GetMapping("/error")
-  public String error(HttpServletRequest request) {
-	String message = (String) request.getSession().getAttribute("error.message");
-	request.getSession().removeAttribute("error.message");
-	return message;
-  }
+@RequestMapping("/webprivate")
+public String private_page() {
+        return "private";
+}
+@RequestMapping("/webpublic")
+public String loginpub() {
+        return "public";
+}
+@RequestMapping("/webadmin")
+public String admin() {
+        return "admin";
+}
 
 }
